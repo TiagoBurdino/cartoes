4f0c669a4ecea2e0352c4eec497a929cd0355c5e
TiagoBurdino <144351015+TiagoBurdino@users.noreply.github.com>
2024-07-12 13:39:25 -0300

    projeto

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
+criaCartao(
+    'Futebol',
+    'Qual o melhor time do Brasil?',
+    'O melhor time do Brasil é o PALMEIRAS!'
+)
+    
+    
